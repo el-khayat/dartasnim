ba6f05e22d7bf9272a28eba7adbbb216a34bdea5
Mohamed Elkhayat <mohamed.el.khayat@oracle.com>
2026-04-25 13:32:04 +0100

    Add GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'react-hot-toast'
